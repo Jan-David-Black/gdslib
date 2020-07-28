518aee858e31bc6404c4eec458d17d0b583cb0a1
joaquin <j>
2020-07-28 08:30:43 -0700

    ring single has some issues

--- gdslib/components/ring_single.py
+++ gdslib/components/ring_single.py
@@ -3,12 +3,13 @@ from simphony.netlist import Subcircuit
 
 from gdslib import plot_circuit
 from gdslib.autoname import autoname
+from gdslib.components.bend_circular import bend_circular
 from gdslib.components.coupler_ring import coupler_ring
 from gdslib.components.waveguide import waveguide
 
 
 @autoname
-def ring_double(
+def ring_single(
     wg_width=0.5,
     gap=0.2,
     length_x=4,
@@ -16,28 +17,22 @@ def ring_double(
     length_y=2,
     coupler=coupler_ring,
     waveguide=waveguide,
+    bend=bend_circular,
 ):
-    """ double bus ring made of two couplers (ct: top, cb: bottom)
-    connected with two vertical waveguides (yl: left, wr: right)
+    """single bus ring made of a ring couplers (cb: bottom)
+    connected with two vertical waveguides (wl: left, wr: right)
+
+    FIXME! it has some issues
 
     .. code::
 
-         --==ct==--
-          |      |
-          wl     wr length_y
-          |      |
-         --==cb==-- gap
-
-          length_x
-
-
-           ---=========---
-        E0    length_x    W0
+              wt (top)
+              length_x
              /         \
             /           \
            |             |
-           N1           N0
-
+           N0           N0
+        wl (left)      wr (right)
            N0            N1
            |             |
             \           /
@@ -52,7 +47,7 @@ def ring_double(
 
       import pp
 
-      c = pp.c.ring_double(wg_width=0.5, gap=0.2, length_x=4, bend_radius=5, length_y=2)
+      c = pp.c.ring_single(wg_width=0.5, gap=0.2, length_x=4, bend_radius=5, length_y=2)
       pp.plotgds(c)
 
 
@@ -61,11 +56,12 @@ def ring_double(
 
         import gdslib as gl
 
-        c = gl.c.ring_double()
+        c = gl.c.ring_single()
         gl.plot_circuit(c)
     """
 
     waveguide = waveguide(length=length_y) if callable(waveguide) else waveguide
+    bend = bend(width=wg_width, bend_radius=bend_radius) if callable(bend) else bend
     coupler = (
         coupler(length_x=length_x, bend_radius=bend_radius, gap=gap, wg_width=wg_width)
         if callable(coupler)
@@ -75,28 +71,35 @@ def ring_double(
     # Create the circuit, add all individual instances
     circuit = Subcircuit("ring_double")
     circuit.add(
-        [(coupler, "ct"), (coupler, "cb"), (waveguide, "wl"), (waveguide, "wr")]
+        [
+            (bend, "bl"),
+            (bend, "br"),
+            (coupler, "cb"),
+            (waveguide, "wl"),
+            (waveguide, "wr"),
+            (waveguide, "wt"),
+        ]
     )
 
     # Circuits can be connected using the elements' string names:
     circuit.connect_many(
         [
             ("cb", "N0", "wl", "E0"),
-            ("wl", "W0", "ct", "N1"),
-            ("ct", "N0", "wr", "E0"),
+            ("wl", "W0", "bl", "N0"),
+            ("bl", "W0", "wt", "W0"),
+            ("wt", "E0", "br", "W0"),
+            ("br", "N0", "wr", "E0"),
             ("wr", "W0", "cb", "N1"),
         ]
     )
     circuit.elements["cb"].pins["W0"] = "input"
     circuit.elements["cb"].pins["E0"] = "output"
-    circuit.elements["ct"].pins["E0"] = "drop"
-    circuit.elements["ct"].pins["W0"] = "cdrop"
     return circuit
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
-    c = ring_double()
+    c = ring_single()
     plot_circuit(c)
     plt.show()
